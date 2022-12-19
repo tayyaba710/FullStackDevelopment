243a5f9a743cb0d495ab292fe69109769734a9e1
Tayyaba Kouser <tayyaba>
2022-12-19 14:45:24 +0530

    uploading once

---
+++ index.js
@@ -0,0 +1,201 @@
+var a = 10;
+// var b = 5;
+// var c = 12;
+// var e = 8;
+// var d;
+
+// d = parseInt((a*(c-b)/e+(b+c))<=(e*(c+a)/(b+c)+a));
+// console.log(d);
+
+// if(d==1){
+//     console.log(parseInt((a*(c-b)/e+(b+c))));
+
+// }
+// else{
+//     console.log(parseInt((e*(c+a)/(b+c)+a)));
+// }
+// var valA ="true";
+// var valB ="false";
+
+// var valC=true;
+// var valD=false;
+
+// console.log(!!valA == !!valB)
+// console.log(!!valC ==!!valD)
+
+// console.log(parseInt('123axy'));
+
+// console.log(Number('123axy'));
+
+// var num1 = 123.4567;
+
+// console.log(num1.toFixed());
+
+// console.log(parseFloat(10));
+
+// var mStr = " Hello World ";
+// mStr.trim();
+// console.log(mStr);
+
+// var mStr = "Mo Tu We Th Fr Sa Su";
+// console.log(mStr.slice(6,10))
+
+// console.log(undefined == null)
+// console.log(undefined === null)
+
+// console.log(!!"false" == !!"true")
+
+// var day = "FR";
+// switch(day){
+//     case "Mo":
+//         console.log("Monday")
+//     case "Tu":
+//         console.log(Tuesday)
+//     case "We":
+//         console.log(Wednesday)
+//     case "Th":
+//         console.log(Thursday)
+//     case "Fr":
+//         console.log(Friday)
+//     case "Sa":
+//         console.log(Saturday)
+//     case "Su":
+//         console.log(Sunday)
+// }
+
+// var str1 = 'Today is';
+// var str2 = '      a beautiful day     '
+// var str3 = ' In Hawaii.     '
+// var result = str1.trim() + " " + str2.trim() + " " + str3.trim();
+
+// console.log("Result: "+result);
+
+// var enteredAlphabet = 'E';
+// enteredAlphabet = enteredAlphabet.toLowerCase();
+
+// // Write a program to check vowel, consonant or non-alphabet using switch case. Change value of enteredAlphabet and check if your code works fine.
+
+// if(enteredAlphabet >= 'a' && enteredAlphabet <= 'z')
+// {
+// 	switch(enteredAlphabet)
+// 	{
+// 		case 'a':
+// 			console.log("Vowel");
+// 			break;
+// 		case 'e':
+// 			console.log("Vowel");
+// 			break;
+// 		case 'i':
+// 			console.log("Vowel");
+// 			break;
+// 		case 'o':
+// 			console.log("Vowel");
+// 			break;
+// 		case 'u':
+// 			console.log("Vowel");
+// 			break;
+// 		default:
+// 			console.log("Consonant");
+// 			break;
+// 	}
+// }
+// else
+// {
+// 	console.log("non-alphabet");
+// }
+// var number_1 = parseInt(prompt("Enter the first value"));
+// var operator = prompt("Enter the operation to perform [+, -, *, /]");
+// var number_2 = parseInt(prompt("Enter the second value"));
+
+// switch(operator)
+// {
+// 	case '+':
+// 		console.log(number_1 + " " + operator + " " + number_2 + " = " + (number_1 + number_2));
+// 		break;
+// 	case '-':
+// 		console.log(number_1 + " " + operator + " " + number_2 + " = " + (number_1 - number_2));
+// 		break;
+// 	case '*':
+// 		console.log(number_1 + " " + operator + " " + number_2 + " = " + (number_1 * number_2));
+// 		break;
+// 	case '/':
+// 		console.log(number_1 + " " + operator + " " + number_2 + " = " + (number_1 / number_2));
+// 		break;
+// 	default:
+// 		console.log("Invalid operation entered");
+// 		break;
+// }
+
+// var output = function(x){
+// 	delete x;
+// 	return x;
+// }
+
+// console.log(output(9));
+
+// var dummy = true;
+
+// console.log(dummy + 0);
+
+// console.log(dummy + "xyz");
+
+// console.log(dummy + true);
+
+// console.log(dummy + false);
+
+// for(var i = 0;i< 5;i++){
+// 	setTimeout(function(){
+// 		console.log(i);
+// 	},1000);
+// }
+
+// var mArr1 = [1,2,3];
+// var mArr2 = [4,5,6];
+
+// mArr1.concat(mArr2);
+// console.log(mArr1);
+
+// var mArr = [10,22,31,47,53];
+// var pos = 5;
+// console.log(mArr[pos]);
+
+// function getSum(){
+// 	console.log(10+20);
+// }
+// console.log(getSum);
+
+// var num1 = 10;
+
+// function getSum(num2){
+// 	var num1 = 20;
+// 	console.log(num1 + num2);
+// }
+
+// getSum(20);
+
+// console.log(num);
+// num=10;
+// console.log(num);
+// var num;
+
+// function getAverage(num1, num2){
+// 	var avg = 0;
+// 	console.log((num1+num2)/2);
+// }
+
+// console.log(avg);
+// getAverage(10,10);
+
+// var count = 0;
+// while(count < 10){
+// 	console.log(count);
+// 	count++;
+// }
+
+// function getSum(){
+// 	var sum = 10+ 20;
+// }
+
+// console.log(getSum());
+
+console.log(10+20);
